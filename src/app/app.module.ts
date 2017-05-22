b03a2962a172d8b21377d980808602e930d88c0d
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-22 09:39:40 +0700

    firebase config in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,19 @@ import { HomePage } from '../pages/home/home';
 import { BillProvider } from '../providers/bill/bill';
 import { AuthProvider } from '../providers/auth/auth';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyC2tAxvLf7beHWC_xQc4HrQqX-r3-Taqzc",
+//   authDomain: "mydebttracker-bf83f.firebaseapp.com",
+//   databaseURL: "https://mydebttracker-bf83f.firebaseio.com",
+//   projectId: "mydebttracker-bf83f",
+//   storageBucket: "mydebttracker-bf83f.appspot.com",
+//   messagingSenderId: "665998928060"
+// };
+
 @NgModule({
   declarations: [
     MyApp,
