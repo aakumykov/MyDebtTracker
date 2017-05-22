03c2f49986a8b1b0500130d8e3f6a5539eb682ec
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-22 14:50:19 +0700

    Страница регистрации

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,24 +1,66 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, Loading, LoadingController, AlertController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
 
-/**
- * Generated class for the SignupPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html',
+selector: 'page-signup',
+templateUrl: 'signup.html',
 })
+
 export class SignupPage {
+	public signupForm: FormGroup;
+	public loading: Loading;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(
+		public navCtrl: NavController, 
+		public loadingCtrl: LoadingController,
+		public alertCtrl: AlertController, 
+		public formBuilder: FormBuilder,
+		public authProvider: AuthProvider
+	) {
+		this.signupForm = formBuilder.group({
+			email: ['', Validators.required],
+			password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+		});
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
-  }
+	/**
+	* If the form is valid it will call the AuthProvider service to sign the
+	* user up password displaying a loading component while the user waits.
+	*
+	* If the form is invalid it will just log the form value,
+	* feel free to handle that as you like.
+	*/
+	signupUser():void {
+		if (!this.signupForm.valid){
+			console.log(this.signupForm.value);
+		} else {
+			this.authProvider.linkAccount(this.signupForm.value.email, this.signupForm.value.password).then(
+				() => {
+					this.loading.dismiss().then( () => {
+						this.navCtrl.pop();
+					});
+				}, 
+				(error) => {
+					this.loading.dismiss().then( () => {
+						var errorMessage: string = error.message;
+						let alert = this.alertCtrl.create({
+							message: errorMessage,
+							buttons: [{
+								text: "Угу",
+								role: 'cancel'
+								}]
+							});
+						alert.present();
+					});
+				}
+			);
+			this.loading = this.loadingCtrl.create();
+			this.loading.present();
+		}
+	}
 
+	
 }
