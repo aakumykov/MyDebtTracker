e5720da67755f07fe62202321136cd135b0593d0
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-22 10:39:42 +0700

    Функции аутентификации.

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -15,4 +15,38 @@ export class AuthProvider {
 			}
 		});
 	}
+
+	getUser():firebase.User { 
+		return this.fireAuth; 
+	}
+
+	loginUser(newEmail: string, newPassword: string):firebase.Promise<any> {
+		return this.afAuth.auth.signInWithEmailAndPassword(newEmail, newPassword);
+	}
+
+	anonymousLogin():firebase.Promise<any> {
+		return this.afAuth.auth.signInAnonymously();
+	}
+
+	linkAccount(email: string, password: string):firebase.Promise<any> {
+		const userProfile = firebase.database().ref('/userProfile');
+		const credential = firebase.auth.EmailAuthProvider.credential(email, password);
+		
+		return firebase.auth().currentUser.link(credential).then(
+			user => {
+				userProfile.child(user.uid).update({ email: email });
+			},
+			error => {
+				console.log("There was an error linking the account", error);
+			}
+		);
+	}
+
+	resetPassword(email: string):firebase.Promise<any> {
+		return this.afAuth.auth.sendPasswordResetEmail(email);
+	}
+
+	logoutUser():firebase.Promise<void> { 
+		return this.afAuth.auth.signOut();
+	}
 }
